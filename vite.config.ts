684b72434b9f5ce812608e9a89d6e19a3c6c144b
agnieszka.konefal <akonewka@gmail.com>
2025-12-23 14:55:41 +0100

    Add config for deploy to GH pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/koneweczka-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
